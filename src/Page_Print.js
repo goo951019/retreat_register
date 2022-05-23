8943a20711fec684c68750facd0770f574529f5d
goo951019 <goo1140@gmail.com>
2022-05-23 19:20:23 -0400

    Updated nametag background

--- src/Page_Print.js
+++ src/Page_Print.js
@@ -7,14 +7,26 @@ import Barcode from 'react-barcode';
 import QRCode from "react-qr-code";
 
 import bg from './images/bg.png';
+import bgBlue from './images/bg-blue.png';
+import bgOrange from './images/bg-orange.png';
 import lodging from './images/lodge.png';
 
-const background = {
-    backgroundImage: `url(${bg})`,
+const backgroundBlue = {
+    backgroundImage: `url(${bgBlue})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'contain',
-    height: '290px',
-    width: '389px',
+    height: '283px',
+    width: '405px',
+    margin : '0',
+    padding: '5px'
+}
+
+const backgroundOrange = {
+    backgroundImage: `url(${bgOrange})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    height: '285px',
+    width: '405px',
     margin : '0',
     padding: '5px'
 }
@@ -31,7 +43,7 @@ const rowPagebreak = {
     pageBreakBefore: 'always'
 }
 const rowPadding = {
-    paddingLeft: '20px'
+    paddingLeft: '10px'
 }
 const rowFirst = {
     paddingTop: '10px'
@@ -82,40 +94,40 @@ function PrintPage() {
                         return (
                             <Row key={index} style={rowStyles}>
                                 <Col xs='auto' style={{paddingLeft:'0', paddingRight:'0'}}>
-                                <Container style={background}>
+                                <Container style={person.group_name==="Pastor"?backgroundOrange:backgroundBlue}>
                                     <h5 style={{marginTop: '20px', marginRight:'3px', textAlign: 'right', color: '#ffffff'}}>{person.church} CHURCH</h5>
                                     <Container style={lodgingImage}/>   
                                     <h6 style={{marginTop: '-42px', marginLeft:'40px', textAlign: 'left'}}><b>{person.kor_sleeping_area===""?"*":person.kor_sleeping_area}</b></h6>
                                     <h6 style={{marginTop: '-7px', marginLeft:'40px', textAlign: 'left'}}><b>{person.eng_sleeping_area===""?"*":person.eng_sleeping_area}</b></h6>
-                                    <Container style={{height: '120px'}}>
+                                    <Container style={{height: '110px'}}>
                                         <h1 style={{lineHeight:'55px',textAlign:'center', letterSpacing: '1px'}}><b>
                                             {person.eng_name === "" ? person.kor_name
                                             :person.kor_name === "" ? person.eng_name
                                             :person.eng_name}<br/>{person.kor_name}
                                         </b></h1>
                                     </Container>
-                                    <Container style={{marginTop:'-23px', marginLeft:'-20px', textAlign:'left'}}>
-                                        <QRCode value={person.barcode} size={40}/>
+                                    <Container style={{marginTop:'-33px', marginLeft:'-20px', textAlign:'left'}}>
+                                        <QRCode value={person.barcode} size={50}/>
                                         {/* Replaced with QRCode<Barcode value={person.barcode} height={30} displayValue={false} width={1} /> */}
                                     </Container>
                                     <h6 style={{marginTop: '23px', marginLeft:'3px', textAlign: 'left', color: '#ffffff', letterSpacing: '-1px'}}>{person.event_name}</h6>
                                 </Container>
                                 </Col>
                                 <Col style={{paddingLeft:'0', paddingRight:'0'}}>
-                                <Container style={background}>
+                                <Container style={person.group_name==="Pastor"?backgroundOrange:backgroundBlue}>
                                     <h5 style={{marginTop: '20px', marginRight:'3px', textAlign: 'right', color: '#ffffff'}}>{person.church} CHURCH</h5>
                                     <Container style={lodgingImage}/>   
                                     <h6 style={{marginTop: '-42px', marginLeft:'40px', textAlign: 'left'}}><b>{person.kor_sleeping_area===""?"*":person.kor_sleeping_area}</b></h6>
                                     <h6 style={{marginTop: '-7px', marginLeft:'40px', textAlign: 'left'}}><b>{person.eng_sleeping_area===""?"*":person.eng_sleeping_area}</b></h6>
-                                    <Container style={{height: '120px'}}>
+                                    <Container style={{height: '110px'}}>
                                     <h1 style={{lineHeight:'55px',textAlign:'center', letterSpacing: '1px'}}><b>
                                         {person.eng_name === "" ? person.kor_name
                                         :person.kor_name === "" ? person.eng_name
                                         :person.eng_name}<br/>{person.kor_name}
                                     </b></h1>
                                     </Container>
-                                    <Container style={{marginTop:'-23px', marginRight:'-20px', textAlign:'right'}}>
-                                        <QRCode value={person.barcode} size={40}/>
+                                    <Container style={{marginTop:'-33px', marginRight:'-20px', textAlign:'right'}}>
+                                        <QRCode value={person.barcode} size={50}/>
                                         {/* Replaced with QRCode<Barcode value={person.barcode} height={30} displayValue={false} width={1} /> */}
                                     </Container>
                                     <h6 style={{marginTop: '23px', marginLeft:'3px', textAlign: 'left', color: '#ffffff', letterSpacing: '-1px'}}>{person.event_name}</h6>
