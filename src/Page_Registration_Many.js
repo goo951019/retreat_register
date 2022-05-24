ce62f89fb9c44d391dce82df66d82249aa33b96b
goo951019 <goo1140@gmail.com>
2022-05-24 18:14:58 -0400

    Added church & sleepign area validation for CSV Import

--- src/Page_Registration_Many.js
+++ src/Page_Registration_Many.js
@@ -4,7 +4,7 @@ import smalltalk from 'smalltalk';
 import { Container, Button } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 
-import {group_KorToEng, sleepingArea_KorToEng} from './Translator';
+import {group_KorToEng} from './Translator';
 
 // addManyParticipants
 async function addManyParticipants(rowData){
@@ -23,7 +23,8 @@ const columns = [
   {dataField: 'kor_name', text: 'Name(Kor)', sort: true, headerStyle: {width: '90px'} },
   {dataField: 'eng_name',text: 'Name(Eng)', sort: true, headerStyle: {width: '130px'}},
   {dataField: 'group_name',text: 'Group', sort: true, headerStyle: {width: '100px'},align:'center'},
-  {dataField: 'language',text: 'Language', sort: true, headerStyle: {width: '100px'}},
+  {dataField: 'language',text: 'Lang', sort: true, headerStyle: {width: '50px'}},
+  {dataField: 'eng_sleeping_area',text: 'Sleeping Area', sort: true, headerStyle: {width: '150px'}},
   {dataField: 'gender',text: 'Gender', sort: true, headerStyle: {width: '65px'}, align:'center'},
   {dataField: 'is_guest',text: 'Guest?', sort: true, headerStyle: {width: '60px'}, align:'center'},
   {dataField: 'fee',text: 'Fee($)', sort: true, headerStyle: {width: '55px'}, align:'center'},
@@ -34,27 +35,33 @@ const columns = [
 function RegistrationManyPage() {
   const navigate = useNavigate();
   const [rowData, setRowData] = useState(null);
+  const [churchList, setChurchList] = useState(null);
+  const [sleepingAreaList, setSleepingAreaList] = useState(null);
+  let churchNotFound = [];
+  let sleepingAreaNotFound = [];
 
   const [file, setFile] = useState();
   const fileReader = new FileReader();
 
   useEffect(() => {
+    let isMounted = true;
+    if(churchList == null) window.api.requestVisibleChurch();
+    if(sleepingAreaList == null) window.api.requestVisibleSleeping_Area();
+    window.api.getVisibleChurch(data => {if(isMounted){setChurchList(data); } });
+    window.api.getVisibleSleeping_Area(data => {if(isMounted){setSleepingAreaList(data); } });
+    return () => {isMounted = false;};
   });
 
   const selectRow = {
-    mode: 'checkbox',
+    mode: 'checkbox', 
     hideSelectColumn: true,
-    clickToSelect: false,
-    bgColor: '#c8e6c9',
-    style: { backgroundColor: '#c8e6c9' },
-    onSelect: (row, isSelect) => {
-    },
-    onSelectAll: (isSelect, rows) => {
-    }
+    clickToSelect: false
   };
 
   const handleAdd = (e) => {
     e.preventDefault();
+    churchNotFound=[];
+    sleepingAreaNotFound=[];
 
     if (file) {
         fileReader.onload = function (event) {
@@ -66,23 +73,38 @@ function RegistrationManyPage() {
             let index = 1;
             rows.map((r)=>{
                 const column = r.split(',');
-                const sleepingArea = column[9]==="수양관"? column[10] 
-                                    : column[9] === "차량" || column[9] === "호텔" || column[9] === "텐트" ? column[9] 
-                                    : column[10];
+                // converting values
+                const churchName = column[0].toUpperCase();
+                const sleepingArea = column[14];
+                const sleepingAreaEng = sleepingAreaList.filter(s => s.kor_sleeping_area === sleepingArea).length !== 0 ? sleepingAreaList.filter(s => s.kor_sleeping_area === sleepingArea)[0].eng_sleeping_area : "";
                 const speakKorean = column[4].toUpperCase().includes("KR") ? "Y" : "N";
                 const speakEnglish = column[4].toUpperCase().includes("EN") ? "Y" : "N";
                 const speakSpanish = column[4].toUpperCase().includes("SP") ? "Y" : "N";
                 const speakChinese = column[4].toUpperCase().includes("CH") ? "Y" : "N";
+
+                // stores church name not found
+                if(!churchNotFound.includes(churchName) && churchList.filter(c => c.church_name === churchName).length === 0) churchNotFound.push(churchName);
+                // stores sleeping area not found
+                if(!sleepingAreaNotFound.includes(sleepingArea) && sleepingAreaList.filter(s => s.kor_sleeping_area === sleepingArea).length === 0){
+                  console.log(column);
+                  sleepingAreaNotFound.push(sleepingArea);
+                }
+                
                 //console.log(column);
+
                 storeData.push({temp_key: index, church: column[0].toUpperCase(), kor_name: column[1], eng_name: column[2], gender: column[3],
                     speak_korean: speakKorean, speak_english: speakEnglish, speak_spanish: speakSpanish, speak_chinese: speakChinese, language: column[4],
                     group_name: group_KorToEng(column[6]), is_guest: column[7]==="TRUE"?"Y":"N", fee: column[8] === '' || column[8] === '-' ? 0 : column[8].replace('$',''),
-                    kor_sleeping_area: sleepingArea, eng_sleeping_area: sleepingArea_KorToEng(sleepingArea), remark: column[11]
+                    kor_sleeping_area: sleepingArea, eng_sleeping_area: sleepingAreaEng, remark: column[11]
                 });
-                index++;
-                return'';
+                index++; return'';
             })
-            setRowData(storeData);
+          
+          if(churchNotFound.length > 0) smalltalk.alert("Wanring","Following church(es) are not found, please adjust it: \n"+ churchNotFound);
+          if(sleepingAreaNotFound.length > 0) smalltalk.alert("Wanring","Following sleeping area(s) are not found, please adjust it: \n"+ sleepingAreaNotFound);
+          
+          // all churches and sleeping areas should be found within the data
+          if(churchNotFound.length === 0 && sleepingAreaNotFound.length === 0) setRowData(storeData);
         };  
         fileReader.readAsText(file);
     }else{
